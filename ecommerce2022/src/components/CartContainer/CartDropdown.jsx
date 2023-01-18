c482ba035c720cfe1d0472eb6e681f35b7d9df6f
Allan Fernandes <Fernzallan14@gmail.com>
2023-01-18 17:20:41 +0000

    cart-dropdown-component

--- ecommerce2022/src/components/CartContainer/CartDropdown.jsx
+++ ecommerce2022/src/components/CartContainer/CartDropdown.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './cart-container.styles.scss';
 import Button from '../Button/button-component'
 import CartItems from '../CartItems/cartItems.component';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cartContext';
+
+
 
 const CartDropdown = () => {
+  const {cartItems} = useContext(CartContext);
+  console.log(cartItems);
   return (
     <div className='cart-dropdown-container'>
       <div className="cart-items">
-        {/* <CartItems cartItems={} /> */}
-      {/* [].map(item => <CartItems cartItems={item}/>)   */}
+      { cartItems.map((item,index) => {return <CartItems key={index} cartItems={item}/>})}
       </div>
       <Button>Go to checkout</Button>
     </div>
--- ecommerce2022/src/contexts/cartContext.jsx
+++ ecommerce2022/src/contexts/cartContext.jsx
@@ -3,17 +3,27 @@ import { useState } from "react";
 
 
 const addItemToCartHelperFn =(cartItems, productToAdd)  => {
+
+
 //check if it already exists in cartItems
 
+const existingCartItem = cartItems.find(item => item.id === productToAdd.id);
 
+// if product exists, increment quantity by 1 
 
-// if it does exists, increase quantity by 1
- 
-
+if(existingCartItem) {
+    return cartItems.map((item) => item.id === productToAdd.id ? {...item, quantity: item.quantity + 1} : {item})
+}
 
 // return cartItems with the product and quantity set to 1
 
-return [{...cartItems, ...productToAdd, quantity: 1}]
+/* first your cartItems starts with []
+then you spread it [...cartItems] because you don't want to lose the existing items
+with the new productToAdd but with an additional quantity of 1
+which transaltes to [...cartItems, {...productToAdd, quantity: 1}]
+[..products] */
+
+return [...cartItems, {...productToAdd, quantity: 1}]
 }
 
 export const CartContext = createContext({
