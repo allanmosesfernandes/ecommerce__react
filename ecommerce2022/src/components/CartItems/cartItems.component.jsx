b86ecd993801881a3d8aed9ab1e8c2cf8bb5e359
Allan Fernandes <Fernzallan14@gmail.com>
2023-01-18 17:35:16 +0000

    cart-dropdown-styles

--- ecommerce2022/src/components/CartItems/cartItems.component.jsx
+++ ecommerce2022/src/components/CartItems/cartItems.component.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './cartItems.style.scss';
 
 const CartItems = ({cartItems}) => {
 
-    const {name, quantity} = cartItems;
+  const { name, quantity, imageUrl, price } = cartItems;
 
   return (
-    <div className='cart-items-container'>
-        <h2>{name}</h2>
-        <span>{quantity}</span>
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">{quantity} x ${price}</span>
+      </div>
+
     </div>
   )
 }
