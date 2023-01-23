33d6e8fcbb36391c9bbc99aa3d479523994d48cc
Allan Fernandes <Fernzallan14@gmail.com>
2023-01-23 14:34:34 +0000

    toggle cart close on redirect

--- ecommerce2022/src/components/CartContainer/CartDropdown.jsx
+++ ecommerce2022/src/components/CartContainer/CartDropdown.jsx
@@ -6,20 +6,28 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cartContext';
 import { useNavigate } from 'react-router-dom';
 
-
 const CartDropdown = () => {
   //==navigation==//
   const navigate = useNavigate();
-  const goToNavigate = () => navigate('/checkout');
+  const goToNavigate = () => {
+    navigate('/checkout')
+    setIsCartOpen(!isCartOpen)
+
+  };
 
   const {cartItems} = useContext(CartContext);
+  
+  //==Toggling Cart Open State ===//
+
+  const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+
   console.log(cartItems);
   return (
     <div className='cart-dropdown-container'>
       <div className="cart-items">
       { cartItems.map((item,index) => {return <CartItems key={index} cartItems={item}/>})}
       </div>
-      <Button onClick={goToNavigate}>Go to checkout</Button>
+      <Button onClick={goToNavigate} >Go to checkout</Button>
     </div>
   )
 }
