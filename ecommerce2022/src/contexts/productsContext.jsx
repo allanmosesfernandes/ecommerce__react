94d5ebd17e8fa2bbe600433788029237d2ad1960
Allan Fernandes <Fernzallan14@gmail.com>
2023-01-25 13:35:46 +0000

    adding-json-to-firebase

--- ecommerce2022/src/contexts/productsContext.jsx
+++ ecommerce2022/src/contexts/productsContext.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import { createContext, useState } from "react";
+import { addCollectionAndDocument } from "../utils/firebase/firebase.utils";
 import PRODUCT_DATA from './products.json'
+import SHOP_DATA from './shop-data.js'
 
 
+console.log(SHOP_DATA)
 export const ProductsContext = createContext({
     products: [],
 })
@@ -9,7 +13,10 @@ export const ProductsContext = createContext({
 
 export const ProductsContextProvider = ({children}) => {
 
-    const [ products,setProducts ] = useState(PRODUCT_DATA);
+    const [ products,setProducts ] = useState([]);
+    // useEffect(() => {
+    //     addCollectionAndDocument("categories", SHOP_DATA);
+    // },[])
     const value = {products}
     
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- ecommerce2022/src/utils/firebase/firebase.utils.js
+++ ecommerce2022/src/utils/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth, signInWithPopup, signInWithEmailAndPassword, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signOut} from "firebase/auth"
-import {getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
+import {getFirestore, doc, getDoc, setDoc , collection, writeBatch} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +15,7 @@ const firebaseConfig = {
   appId: "1:281539300655:web:666bf948f784ff12baa5da"
 };
 
-// Initialize Firebase
+//=== Initialize Firebase ===//
 export const app = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
@@ -24,15 +24,28 @@ provider.setCustomParameters({
     prompt: "select_account"
 })
 
-// == Firebase Auth == //
+//=== Firebase Auth ===//
 export const auth = getAuth();
 
 export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
 
-// == Firebase Database ==//
+//=== Firebase Database ===//
 
 export const db = getFirestore();
+//=== FIrestore Databse Adding Collection & Documents ===//
+export const addCollectionAndDocument = async (collectionName, objectsToAdd) => {
+    const batch = writeBatch(db);
+    const collectionRef = collection(db, collectionName);
+    
 
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    })
+
+    await batch.commit();
+    console.log('done'); 
+}
 export const createUserDocFromAuth = async (users, additionalInformation={}) => {
     const userDocRef = doc(db, "users", users.uid);
     const userSnapShot = await getDoc(userDocRef);
@@ -64,7 +77,7 @@ export const createAuthUserEmailPassword = async (email, password) => {
 }
 
 
-//======== Sign In Already existing user=========//
+//======== Sign In Already existing user =========//
 export const signInAuthUserEmailPassword = async (email,password) => {
     if(!email || !password) return;
     return await signInWithEmailAndPassword(auth, email, password);
@@ -75,5 +88,17 @@ export const signInAuthUserEmailPassword = async (email,password) => {
 export const signOutAuthUser = async () => await signOut(auth);
 
 
-// === On Auth State Changed Listener === //
+//====  On Auth State Changed Listener ===//
 export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+
+
+
+
+
+
+
+
+
+
+
+
