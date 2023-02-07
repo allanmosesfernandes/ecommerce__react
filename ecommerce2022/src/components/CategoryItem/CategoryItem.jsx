e9c174ee5e9f23d931b25c560fb87ad9fbfb1ec5
Allan Fernandes <Fernzallan14@gmail.com>
2023-02-07 11:36:03 +0000

    routes homepage

--- ecommerce2022/src/components/CategoryItem/CategoryItem.jsx
+++ ecommerce2022/src/components/CategoryItem/CategoryItem.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const CategoryItem = ({product}) => {
-const {id, imageUrl, title} = product;
-  return (<div className='category-container' key={id}>
+const {id, imageUrl, title, route} = product;
+const navigate = useNavigate();
+const onNavigateHandler = () => {
+  navigate(route);
+}
+
+
+  return (<div className='category-container' key={id} onClick={onNavigateHandler}>
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
             <div className='category-body-container'>
             <h2>{title}</h2>
